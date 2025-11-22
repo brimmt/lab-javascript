13078350fbe64ab4b7e89f49581ea836d27677b7
Tatiana Brimm <tbrimm1@student.gsu.edu>
2025-11-21 20:48:16 -0500

    Practiced forEach

---
+++ foundations/for_each.js
@@ -0,0 +1,11 @@
+const socials = ['Twitter', 'LinkedIn', 'Facebook', 'Instagram'];
+
+console.log(socials);
+
+socials.forEach(function (item) {
+    console.log(item);
+})
+
+// arrow version
+
+socials.forEach((item) => console.log(item));
